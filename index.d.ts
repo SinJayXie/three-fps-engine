58c15784270ab643d4ed0ef358c94135dd8847db
xiexianzhang <836197970@qq.com>
2023-05-04 16:35:31 +0800

    first commit

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import * as THREE from 'three'
+import CoreEngine from "./lib/Libs/CoreEngine";
+import ModelGenerate from "./lib/Libs/ModelGenerate";
+const MathUtils = THREE.MathUtils
+
+export {
+    THREE,
+    CoreEngine,
+    MathUtils,
+    ModelGenerate
+}
